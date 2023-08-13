391bc86ba9cfcafa3b1e860284e7f160f093280d
Administrator <1-root@users.noreply.git.arpco.xyz>
2023-08-13 22:59:49 +0100

    aded gitlab link on the footer

--- foot.php
+++ foot.php
@@ -1,14 +1,14 @@
 <div class="footer">
     <div class="container">
         <div class="row">
-            <div class="col text-center">
-                
+            <div class="col text-center" style="padding-top:2px">
+                <a href="https://git.ajrich.co.uk/web/inventory" class="link" style="font-size:12px" target="_blank">GitLab</a>
             </div>
             <div class="col-6 text-center">
                 Copyright &copy;<?php echo date("Y"); ?> Andrew Richardson. All rights reserved.
             </div>
-            <div class="col text-center">
-            <a href="https://todo.ajrich.co.uk/#/board/16" class="link" style="font-size:12px" target="_blank">Road Map</a>
+            <div class="col text-center" style="padding-top:2px">
+                <a href="https://todo.ajrich.co.uk/#/board/16" class="link" style="font-size:12px" target="_blank">Road Map</a>
             </div>
         </div>
     </div>

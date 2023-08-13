749be5d4ca42e58965637d34bd94f6f52d422860
Administrator <1-root@users.noreply.git.arpco.xyz>
2023-08-13 22:49:59 +0100

    changed font size on link

--- foot.php
+++ foot.php
@@ -8,7 +8,7 @@
                 Copyright &copy;<?php echo date("Y"); ?> Andrew Richardson. All rights reserved.
             </div>
             <div class="col text-center">
-            <a href="https://todo.ajrich.co.uk/#/board/16" id="todo" class="link" target="_blank">Road Map</a>
+            <a href="https://todo.ajrich.co.uk/#/board/16" class="link" style="font-size:12px" target="_blank">Road Map</a>
             </div>
         </div>
     </div>

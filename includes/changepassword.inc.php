f1a45be36fe1a98abf5199eadda00909703e2983
Administrator <1-root@users.noreply.git.arpco.xyz>
2023-08-28 14:05:59 +0100

    fixed the url for password reset confirmation email

--- includes/changepassword.inc.php
+++ includes/changepassword.inc.php
@@ -172,8 +172,8 @@ if (isset($_POST['password-submit'])) { // normal change password requests
             $selector = $_POST["selector"];
 	        $validator = $_POST["validator"];
 
-            $redirect_query = "?selector=".$selector."&validator=".$validator;
             $url = "../reset-password.php$redirect_query";
+            $baseUrl = ((str_contains($_SERVER['HTTP_REFERER'], "https")) ? 'https' : 'http') . "://".$current_base_url."/reset-password.php";
 
             if (isset($_POST["password"]) && isset($_POST["password_confirm"])) {
 
@@ -256,9 +256,9 @@ if (isset($_POST['password-submit'])) { // normal change password requests
 
                                             include 'smtp.inc.php';
 
-                                            $email_subject = ucwords($current_system_name)." - Reset your password";
-                                            $email_body = '<p>We recieved a password reset request for your account. <br>The link to reset your password is below. If you did not make this request, you can ignore this email.</p>
-                                                            <p>Here is your password reset link: </br><a href="' . $url . '">' . $url . '</a></p>';
+                                            $email_subject = ucwords($current_system_name)." - Password Reset!";
+                                            $email_body = '<p>Your password has been reset.</p>
+                                                            <p>Click <a href="' . $baseUrl . '">here</a> to login.</p>';
                                             send_email($user_email, $user_fullname, $config_smtp_from_name, $email_subject, createEmail($email_body));
                                             header("Location: ../login.php?newpwd=passwordupdated");
                                         }

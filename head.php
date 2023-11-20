305a626ad8cbe4ed0fef6f94f34c8e9e8d68cdcc
Administrator <1-root@users.noreply.git.arpco.xyz>
2023-11-20 00:59:30 +0000

    updated the version number

--- head.php
+++ head.php
@@ -5,7 +5,7 @@
 // You should have received a copy of the GNU General Public License along with StockBase. If not, see <https://www.gnu.org/licenses/>.
 
 // PAGE HEADER SETUP - SETS UP CSS, BOOTSTRAP AND OTHER STYLES AND SCRIPTS
-$versionNumber = 'v0.4.2-beta';
+$versionNumber = 'v0.5.0-beta';
 
 include './includes/get-config.inc.php'; // get config options
 

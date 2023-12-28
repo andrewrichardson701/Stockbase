dd2d46031bbe7dc372501809ee24e37862678aea
Administrator <1-root@users.noreply.git.arpco.xyz>
2023-12-28 13:15:02 +0000

    correct dbname

--- hidden/dbh.inc.php
+++ hidden/dbh.inc.php
@@ -9,7 +9,7 @@
 $servername = 'localhost';
 $dBUsername = 'admin';
 $dBPassword = 'admin';
-$dBName = 'stockbase_copy_2';
+$dBName = 'stockbase';
 
 $conn = mysqli_connect($servername, $dBUsername, $dBPassword, $dBName);
 

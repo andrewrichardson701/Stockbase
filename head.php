738f3a5377145a33849583810bbea2e20090a582
root <Andrew Richardson>
2024-12-19 08:16:04 +0000

    version number changed to 1.3.0

--- head.php
+++ head.php
@@ -5,7 +5,7 @@
 // You should have received a copy of the GNU General Public License along with StockBase. If not, see <https://www.gnu.org/licenses/>.
 
 // PAGE HEADER SETUP - SETS UP CSS, BOOTSTRAP AND OTHER STYLES AND SCRIPTS
-$versionNumber = 'v1.2.3';
+$versionNumber = 'v1.3.0';
 
 include './includes/get-config.inc.php'; // get config options
 

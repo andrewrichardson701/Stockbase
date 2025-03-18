d8bf4afe54a053b7ed9a95b01109efe42eac0d98
root <Andrew Richardson>
2025-03-18 16:24:28 +0000

    added the formatting around the transactions include

--- resources/views/transactions.blade.php
+++ resources/views/transactions.blade.php
@@ -2,7 +2,7 @@
 <html lang="en">
 <head>
     @include('head')
-    <title>{{$head_data['config_compare']['system_name']}} - Stock</title>
+    <title>{{$head_data['config_compare']['system_name']}} - Transactions</title>
 </head>
 <body>
     <!-- Header and Nav -->
@@ -10,7 +10,12 @@
     <!-- End of Header and Nav -->
 
     <div class="content">
-        @include('includes.stock.transactions')
+        <div class="container" style="padding-bottom:25px">
+            <h2 class="header-small" style="padding-bottom:5px">Transactions - <a class="link" href="{{ url('stock') }}/{{ $params['stock_id'] }}">{{ $stock_data['name'] }}</a> - Stock ID: {{ $params['stock_id'] }} @if ($stock_data['is_cable'] == 1)  (cable)@endif</h2>
+        </div>
+        <div class="container">
+            @include('includes.stock.transactions')
+        </div>
     </div>
     
     @include('foot')

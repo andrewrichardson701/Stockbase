24bf52d71d5c07cbd46bb9daba3ff97f5a860a6d
root <Andrew Richardson>
2025-03-23 08:49:53 +0000

    fixed the link urls

--- resources/views/assets.blade.php
+++ resources/views/assets.blade.php
@@ -30,30 +30,30 @@
 
         <div class="container">
             <div class="row ">
-                <div class="col text-center well-nopad theme-divBg @if (in_array($head_data['user']['role_id'], [0, 2]))  clickable" onclick="navPage('optics.php')" @else  no-perms" title="Not permitted" @endif
+                <div class="col text-center well-nopad theme-divBg @if (in_array($head_data['user']['role_id'], [0, 2]))  clickable" onclick="navPage(`{{ url('optics') }}`)" @else  no-perms" title="Not permitted" @endif
                 style="margin:5px">
                     <h4>Optics</h4>
                     <img style="max-width:100px;overflow:hidden;" src="/img/stock/SFP.png">
                 </div>
-                <div class="col text-center well-nopad theme-divBg clickable" style="margin:5px" onclick="navPage('CPUs.php')">
+                <div class="col text-center well-nopad theme-divBg clickable" style="margin:5px" onclick="navPage(`{{ url('cpus') }}`)">
                     <h4>CPUs</h4> 
                     <img style="max-width:100px;overflow:hidden;" src="/img/stock/CPU.png">
                 </div>
-                <div class="col text-center well-nopad theme-divBg no-perms" style="margin:5px" onclick="navPage('optics.php')" title="Coming soon...">
+                <div class="col text-center well-nopad theme-divBg no-perms" style="margin:5px" onclick="navPage(`{{ url('memory') }}`)" title="Coming soon...">
                     <h4>Memory</h4> 
                     <img style="max-width:100px;overflow:hidden;" src="/img/stock/RAM.png">
                 </div>
             </div>
             <div class="row ">
-                <div class="col text-center well-nopad theme-divBg no-perms" style="margin:5px" onclick="navPage('optics.php')" title="Coming soon...">
+                <div class="col text-center well-nopad theme-divBg no-perms" style="margin:5px" onclick="navPage(`{{ url('disks') }}`)" title="Coming soon...">
                     <h4>Disks</h4>
                     <img style="max-width:100px;overflow:hidden;" src="/img/stock/HDD.png">
                 </div>
-                <div class="col text-center well-nopad theme-divBg no-perms" style="margin:5px" onclick="navPage('optics.php')" title="Coming soon...">
+                <div class="col text-center well-nopad theme-divBg no-perms" style="margin:5px" onclick="navPage(`{{ url('fans') }}`)" title="Coming soon...">
                     <h4>Fans</h4> 
                     <img style="max-width:100px;overflow:hidden;" src="/img/stock/Fan.png">
                 </div>
-                <div class="col text-center well-nopad theme-divBg no-perms" style="margin:5px" onclick="navPage('optics.php')" title="Coming soon...">
+                <div class="col text-center well-nopad theme-divBg no-perms" style="margin:5px" onclick="navPage(`{{ url('psus') }}`)" title="Coming soon...">
                     <h4>PSUs</h4> 
                     <img style="max-width:100px;overflow:hidden;" src="/img/stock/PSU.png">
                 </div>

3678f10d048978f5bdee558f7f7d59f2007a258d
root <Andrew Richardson>
2025-04-08 13:37:49 +0100

    added some extra options for user management

--- app/Http/Requests/ProfileUpdateRequest.php
+++ app/Http/Requests/ProfileUpdateRequest.php
@@ -25,6 +25,7 @@ class ProfileUpdateRequest extends FormRequest
                 'max:255',
                 Rule::unique(User::class)->ignore($this->user()->id),
             ],
+            'theme_id' => ['required', 'int'],
         ];
     }
 }

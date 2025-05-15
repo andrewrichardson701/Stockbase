a07d98171c8504714e9effcccc872bc5681675e2
Administrator <1-root@users.noreply.git.arpco.xyz>
2025-05-15 16:09:58 +0100

    migration for the theme table added

---
+++ database/migrations/2025_01_10_145619_add_timestamps.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('theme', function (Blueprint $table) {
+            //
+            $table->timestamps(); // Adds 'created_at' and 'updated_at' columns
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('theme', function (Blueprint $table) {
+            //
+            $table->dropColumn(['created_at', 'updated_at']);
+        });
+    }
+};

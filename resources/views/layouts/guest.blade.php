a0b08539f4e286e959edbd58012c1bc3a6af5988
Administrator <1-root@users.noreply.git.arpco.xyz>
2025-07-14 10:31:14 +0100

    added the favicon to the guest page for login showing the correct icon

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -6,7 +6,8 @@
         <meta name="csrf-token" content="{{ csrf_token() }}">
 
         <title>{{ config('app.name', 'Laravel') }}</title>
-
+        <link rel="icon" type="image/png" href="{{ asset('img/config/'. $head_data['config_compare']['favicon_image']) }}"
+        
         <!-- Fonts -->
         <link rel="stylesheet" href="css/bootstrap-basic-footer.css">
         <link rel="stylesheet" href="css/inv.css">

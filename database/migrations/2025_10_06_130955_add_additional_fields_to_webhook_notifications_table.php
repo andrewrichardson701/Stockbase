23c7fe640cee9d48a2ca3c33683e6e5768cb0a05
Administrator <1-root@users.noreply.git.arpco.xyz>
2025-10-06 18:09:42 +0100

    fixed a migration

--- database/migrations/2025_10_06_130955_add_additional_fields_to_webhook_notifications_table.php
+++ database/migrations/2025_10_06_130955_add_additional_fields_to_webhook_notifications_table.php
@@ -13,7 +13,7 @@ return new class extends Migration
     {
         Schema::table('webhook_notifications', function (Blueprint $table) {
             //
-            $table->integer('webhook_template_id');
+            $table->integer('webhook_template_id')->nullable();
             $table->timestamps();
         });
     }

47e0eb7be512ef02091d5c403f9b5082b9348d7a
Administrator <1-root@users.noreply.git.arpco.xyz>
2026-03-03 07:22:57 +0000

    now allows a field to be missing, and sets it to null if it is

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -351,7 +351,7 @@ class AdminController extends Controller
 
         if (isset($request['smtp-submit']) || isset($request['smtp-restore-defaults'])) {
             if ($request['_token'] == csrf_token()) {
-                $request->validate([
+                $validated = $request->validate([
                         'smtp_host' => 'string|required',
                         'smtp_port' => 'integer|required',
                         'smtp_encryption' => 'string|required',
@@ -366,7 +366,21 @@ class AdminController extends Controller
                         'smtp_from_name' => 'string|required',
                         'smtp_to_email' => 'string|required',
                 ]);
-                return AdminModel::updateConfigSettings($request->input());
+
+                // set any nullable fields to null if empty
+                $validated = array_merge(
+                    array_fill_keys([
+                        'smtp_client_id',
+                        'smtp_client_secret',
+                        'smtp_oauth_provider',
+                        'smtp_refresh_token',
+                        'smtp_username',
+                        'smtp_password',
+                    ], null),
+                    $validated
+                );
+
+                return AdminModel::updateConfigSettings($validated);
             } else {
                 return 'Error: CSRF token missmatch.';
             }

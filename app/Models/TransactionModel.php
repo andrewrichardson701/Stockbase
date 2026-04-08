79fbe1ee0725a32114c72028d0a41e7415b7d45d
Administrator <1-root@users.noreply.git.arpco.xyz>
2026-04-08 14:14:54 +0100

    added a Transaction function for disks

--- app/Models/TransactionModel.php
+++ app/Models/TransactionModel.php
@@ -264,4 +264,16 @@ class TransactionModel extends Model
             return ['error' => 'non-numeric id'];
         }
     }
+
+    static public function addDiskTransaction($request)
+    {
+        $insert = DB::table('disk_transaction')->insertGetId($request);
+        $id = $insert;
+
+        if (is_numeric($id)) {
+            return ['success' => $id];
+        } else {
+            return ['error' => 'non-numeric id'];
+        }
+    }
 }

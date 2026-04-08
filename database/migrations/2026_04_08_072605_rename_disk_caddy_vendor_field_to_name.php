0961d3ee85da2718148aa3682b79fda34edd9672
Administrator <1-root@users.noreply.git.arpco.xyz>
2026-04-08 14:43:05 +0100

    disk_transaction table added

--- database/migrations/2026_04_08_072605_rename_disk_caddy_vendor_field_to_name.php
+++ database/migrations/2026_04_08_072605_rename_disk_caddy_vendor_field_to_name.php
@@ -4,25 +4,18 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
-    /**
-     * Run the migrations.
-     */
+return new class extends Migration {
     public function up(): void
     {
-        Schema::table('name', function (Blueprint $table) {
-            //
+        Schema::table('disk_caddy', function (Blueprint $table) {
+            $table->renameColumn('vendor', 'name');
         });
     }
 
-    /**
-     * Reverse the migrations.
-     */
     public function down(): void
     {
-        Schema::table('name', function (Blueprint $table) {
-            //
+        Schema::table('disk_caddy', function (Blueprint $table) {
+            $table->renameColumn('name', 'vendor');
         });
     }
 };

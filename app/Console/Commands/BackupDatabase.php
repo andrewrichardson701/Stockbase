4e7d7ac6256a0ff8bb14df7086f9f7071dcf8591
Administrator <1-root@users.noreply.git.arpco.xyz>
2026-04-17 11:09:08 +0100

    added a backup command for the database

---
+++ app/Console/Commands/BackupDatabase.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+
+class BackupDatabase extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'backup:database';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Command description';
+
+    /**
+     * Execute the console command.
+     */
+    public function handle()
+    {
+        $filename = storage_path('app/backups/stockbase_db_backup_full_' . now()->format('Y-m-d_H-i-s') . '.sql');
+
+        $command = sprintf(
+            'mysqldump -u%s -p%s %s > %s',
+            config('database.connections.mysql.username'),
+            config('database.connections.mysql.password'),
+            config('database.connections.mysql.database'),
+            $filename
+        );
+
+        exec($command);
+
+        $this->info('Backup created: ' . $filename);
+    }
+}

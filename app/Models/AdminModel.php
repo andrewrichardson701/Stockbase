e15838896d7310a3bb2d8e337c77f08b5b5e909a
Administrator <1-root@users.noreply.git.arpco.xyz>
2026-04-20 12:12:35 +0100

    saving the global settings now works properly

--- app/Models/AdminModel.php
+++ app/Models/AdminModel.php
@@ -269,6 +269,7 @@ class AdminModel extends Model
         $user = GeneralModel::getUser();
         $config_fields = Schema::getColumnListing('config');
         $excluded_keys = ['_token', 'global-submit', 'smtp-submit', 'ldap-submit', 'webhook-submit'];
+        $not_null_keys = ['system_name', 'SKU_prefix', 'base_url', 'banner_color', 'currency'];
 
         if (isset($data['global-submit'])) {
             $anchor = 'global-settings';
@@ -377,27 +378,30 @@ class AdminModel extends Model
                 }
 
                 if ($current_data->$field !== $value) {
-                    //update needed
-                    if (filled($value) || $value == null) {
 
-                        $update = DB::table('config')->where('id', 1)->update([$field => $value, 'updated_at' => now()]);
-                        
-                        if ($update) {
-                            // changelog needed
-                            $changelog_info['field'] = $field;
-                            $changelog_info['previous_value'] = $current_data->$field;
-                            $changelog_info['new_value'] = $value;
+                    if (!in_array($field, $not_null_keys) || ($value !== null && $value !== '')) { // make sure the value is not "" and not null
+                        //update needed
+                        if (filled($value) || $value == null) {
 
-                            $changed[$field] = ['current' => $current_data->$field, 'new_data' => $value, 'reason' => 'updated'];
+                            $update = DB::table('config')->where('id', 1)->update([$field => $value, 'updated_at' => now()]);
+                            
+                            if ($update) {
+                                // changelog needed
+                                $changelog_info['field'] = $field;
+                                $changelog_info['previous_value'] = $current_data->$field;
+                                $changelog_info['new_value'] = $value;
 
-                            GeneralModel::updateChangelog($changelog_info);
+                                $changed[$field] = ['current' => $current_data->$field, 'new_data' => $value, 'reason' => 'updated'];
+
+                                GeneralModel::updateChangelog($changelog_info);
+                            } else {
+                                // failed to update DB
+                                $errors[$field] = 'failed to update';
+                                $unchanged[$field] = ['current' => $current_data->$field, 'new_data' => $value, 'reason' => 'unable to push data to DB'];
+                            }
                         } else {
-                            // failed to update DB
-                            $errors[$field] = 'failed to update';
-                            $unchanged[$field] = ['current' => $current_data->$field, 'new_data' => $value, 'reason' => 'unable to push data to DB'];
+                            $unchanged[$field] = ['current' => $current_data->$field, 'new_data' => $value, 'reason' => 'null data'];
                         }
-                    } else {
-                        $unchanged[$field] = ['current' => $current_data->$field, 'new_data' => $value, 'reason' => 'null data'];
                     }
                 } else {
                     $unchanged[$field] = ['current' => $current_data->$field, 'new_data' => $value, 'reason' => 'matching data'];

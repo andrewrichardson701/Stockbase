5414c9546f9caeebd1a94c79b8b7017f108a7ddd
Administrator <1-root@users.noreply.git.arpco.xyz>
2026-06-08 12:54:06 +0100

    added the missing migration

--- database/migrations/2026_06_02_130022_add_quantity_to_memory_item_table.php
+++ database/migrations/2026_06_02_130022_add_quantity_to_memory_item_table.php
@@ -4,25 +4,25 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration {
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
     public function up(): void
     {
-        Schema::create('memory_transaction', function (Blueprint $table) {
-            $table->bigIncrements('id');
-            $table->text('table_name');
-            $table->bigInteger('item_id');
-            $table->text('type');
-            $table->text('reason');
-            $table->date('date');
-            $table->time('time');
-            $table->text('username');
-            $table->bigInteger('shelf_id');
-            $table->timestamps(); // created_at & updated_at (nullable)
+        Schema::table('memory_item', function (Blueprint $table) {
+            $table->integer('quantity')->default(0);
         });
     }
 
+    /**
+     * Reverse the migrations.
+     */
     public function down(): void
     {
-        Schema::dropIfExists('memory_transaction');
+        Schema::table('memory_item', function (Blueprint $table) {
+            $table->dropColumn('quantity');
+        });
     }
 };
